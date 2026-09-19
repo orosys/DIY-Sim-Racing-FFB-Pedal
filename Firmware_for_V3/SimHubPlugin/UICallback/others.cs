2c29dda9e925cd1e4567b00fe0388f2edd3577b2
orosys <orosys@gmail.com>
2026-09-19 16:22:39 +0900

    fix(simhub): restore profile effect states from pedal configs

--- Firmware_for_V3/SimHubPlugin/UICallback/others.cs
+++ Firmware_for_V3/SimHubPlugin/UICallback/others.cs
@@ -1046,7 +1046,6 @@ namespace User.PluginSdkDemo
 
 
 
-            updateTheGuiFromConfig();
         }
         public void Profile_change(uint profile_index)
         {
@@ -1095,7 +1094,7 @@ namespace User.PluginSdkDemo
                                 Plugin.Settings.RPM_enable_flag[j] = 1;
                                 break;
                             case 2:
-                                //Plugin.Settings. = 1;
+                                dap_config_st[j].payloadPedalConfig_.BP_trigger = 1;
                                 break;
                             case 3:
                                 Plugin.Settings.G_force_enable_flag[j] = 1;
@@ -1125,7 +1124,7 @@ namespace User.PluginSdkDemo
                                 Plugin.Settings.RPM_enable_flag[j] = 0;
                                 break;
                             case 2:
-                                //Plugin.Settings. = 1;
+                                dap_config_st[j].payloadPedalConfig_.BP_trigger = 0;
                                 break;
                             case 3:
                                 Plugin.Settings.G_force_enable_flag[j] = 0;
@@ -1149,6 +1148,7 @@ namespace User.PluginSdkDemo
             }
             //effect profile change
 
+            updateTheGuiFromConfig();
         }
 
         public void DelayCall(int msec, Action fn)
